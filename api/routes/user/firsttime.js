a52dd1ecb479252e8cdd79851fff79f774b2df71
Abhinav Krishna C K <me@abhy.me>
2019-01-25 08:32:36 +0530

    add firsttime check to update

--- api/routes/user/firsttime.js
+++ api/routes/user/firsttime.js
@@ -1,19 +1,51 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    firstTime = true; //TODO check from db
-    if (firstTime) {
-        // TODO set the firsttime to false in DB
-        res.status(200).json({
-            isFirsttime: true,
-            message:
-                'Change the default password by visiting user/setpassword route',
+const User = require('../../models/user');
+
+// Auth check
+const CheckAuth = require('../../middleware/check-auth');
+
+router.get('/', CheckAuth, (req, res) => {
+    User.findOne({ penno: req.user.penno }, function(err, result) {
+        if (err) {
+            return res.status(500).json({ error: err });
+        }
+        if (result) {
+            console.log(result.firsttime);
+            if (result.firsttime) {
+                console.log(result);
+                // TODO set the firsttime to false in DB
+                User.findOneAndUpdate(
+                    { penno: req.user.penno },
+                    { firsttime: false },
+                    { upsert: true },
+                    function(err, doc) {
+                        if (err) {
+                            console.log('error occoured');
+                            return res.status(500).json({ error: err });
+                        }
+                        // return res.status(201).json({
+                        //     message: 'Status set',
+                        // });
+                    }
+                );
+                return res.status(200).json({
+                    isFirsttime: true,
+                    message:
+                        'Change the default password by visiting user/setpassword route, and enter previous stations',
+                });
+            } else {
+                return res.status(200).json({
+                    isFirsttime: false,
+                });
+            }
+        }
+        return res.status(404).json({
+            error: {
+                message: "Profile couldn't be fetched",
+            },
         });
-    } else {
-        res.status(200).json({
-            isFirsttime: false,
-        });
-    }
+    });
 });
 
 module.exports = router;
